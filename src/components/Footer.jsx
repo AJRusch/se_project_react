a0805543b06764627e70b13bb3d393ab049ec63a
AJRusch <155108459+AJRusch@users.noreply.github.com>
2024-07-09 18:13:55 -0500

    fixed id error

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../blocks/footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer footer-head">
+      <p className="footer__copyright">Developed by Austin Rusch</p>
+      <p className="footer__copy-year">2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
