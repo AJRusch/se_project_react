ac59f613ff74ae6b7c2151125e6e406c488892b6
AJRusch <155108459+AJRusch@users.noreply.github.com>
2024-07-24 13:44:26 -0500

    Add files via upload
    fixed additem modal open issue

---
+++ src/utils/api.js
@@ -0,0 +1,42 @@
+const baseUrl = "http://localhost:3001";
+const headers = { "Content-Type": "application/json" };
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+}
+
+export { checkResponse };
+
+function getItems() {
+  return fetch(`${baseUrl}/items`, {
+    headers: headers,
+  }).then(checkResponse);
+}
+
+export { getItems };
+
+function postItems(name, imageUrl, weather) {
+  return fetch(`${baseUrl}/items`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({
+      name,
+      imageUrl,
+      weather,
+    }),
+  }).then(checkResponse);
+}
+
+export { postItems };
+
+function deleteItems(item) {
+  return fetch(`${baseUrl}/items/${item._id}`, {
+    method: "DELETE",
+    headers: headers,
+  }).then(checkResponse);
+}
+
+export { deleteItems };
