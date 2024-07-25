e58906d15dce6e01201615899be38fe61927a400
AJRusch <155108459+AJRusch@users.noreply.github.com>
2024-07-25 12:20:42 -0500

    Add files via upload
    fixed styling issue with itemmodal and additemmodal

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import avatar from "../assets/user__avatar.png";
+import "../blocks/sidebar.css";
+
+function Sidebar() {
+  return (
+    <>
+      <div className="sidebar">
+        <img src={avatar} alt="Basic Avatar" className="sidebar__avatar" />
+        <p className="sidebar__username">Username</p>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
