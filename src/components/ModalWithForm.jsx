380567b7f693c7204d7541e27628eba84e1eea08
AJRusch <155108459+AJRusch@users.noreply.github.com>
2024-09-13 12:37:48 -0500

    Add files via upload

---
+++ src/components/ModalWithForm.jsx
@@ -0,0 +1,42 @@
+import "../blocks/modalWithForm.css";
+
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  isOpen,
+  onClose,
+  onSubmit,
+  redirectText,
+  redirectTextClick,
+}) {
+  return (
+    <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button
+          type="button"
+          className="modal__close"
+          onClick={onClose}
+        ></button>
+        <form onSubmit={onSubmit} className="modal__form">
+          {children}
+          <div className="modal__btn-box">
+            <button type="submit" className="modal__submit">
+              {buttonText}
+            </button>
+            <button
+              type="button"
+              className="modal__btn-redirect"
+              onClick={redirectTextClick}
+            >
+              {redirectText}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
